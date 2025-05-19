c7172459fd3282a3c071cbf5604d63d4a4877c54
Sergio Junqueira <sergiojunqueira.s@gmail.com>
2025-05-19 09:35:32 -0300

    stack bg-fix

--- src/app/components/stackPage/page.tsx
+++ src/app/components/stackPage/page.tsx
@@ -8,7 +8,7 @@ export default function TechStack() {
           Stack Principal
         </h2>
         <div className="flex flex-wrap justify-center gap-1">
-          <span className="hover:border-primary-500 mx-auto flex gap-2 rounded-4xl border border-transparent bg-green-400 px-4 py-2 text-neutral-900 transition-all duration-200 ease-in-out hover:scale-[1.03] hover:bg-neutral-200 dark:bg-neutral-800 dark:text-neutral-100 dark:hover:bg-neutral-700">
+          <span className="hover:border-primary-500 mx-auto flex gap-2 rounded-4xl border border-transparent bg-neutral-100 px-4 py-2 text-neutral-900 transition-all duration-200 ease-in-out hover:scale-[1.03] hover:bg-neutral-200 dark:bg-neutral-800 dark:text-neutral-100 dark:hover:bg-neutral-700">
             <Icon icon="simple-icons:nodedotjs" width="24" height="24" />
             NodeJs
           </span>
