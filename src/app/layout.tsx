65732069283ec099c0c339018754cd0c37e75e1f
Sergio Junqueira <sergiojunqueira.s@gmail.com>
2025-05-29 12:51:07 -0300

    chore: vercel insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header/page";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import { ThemeProvider } from "./components/themeProvider/themeProvider";
 import Footer from "./components/Footer/page";
@@ -36,6 +37,7 @@ export default function RootLayout({
           {children}
           <Footer />
         </ThemeProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
