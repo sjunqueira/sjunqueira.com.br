2f9cf099f199b4d11a0a67bfbe6ec9eb8aefb0ad
Sergio Junqueira <sergiojunqueira.s@gmail.com>
2026-07-03 22:20:52 -0300

    feat: Bump React version to "16.3.0"

--- proxy.ts
+++ proxy.ts
@@ -1,16 +1,16 @@
+// src/proxy.ts
 import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
 import { NextRequest, NextResponse } from "next/server";
 
 const handleI18nRouting = createMiddleware(routing);
 
-export default function middleware(request: NextRequest) {
+export default function proxy(request: NextRequest) {
   if (request.nextUrl.pathname === "/") {
     return NextResponse.redirect(
       new URL(`/${routing.defaultLocale}`, request.url)
     );
   }
-
   return handleI18nRouting(request);
 }
 
