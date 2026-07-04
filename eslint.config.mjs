86a763265376f43f4688e4153ab6cb68b7f78f0f
Sergio Junqueira <sergiojunqueira.s@gmail.com>
2026-07-03 22:25:52 -0300

    feat: bump next version to "16.2.10"

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,20 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.config({
-    extends: ["next/core-web-vitals", "next/typescript"],
-    rules: {
-      "react/no-unescaped-entities": "off",
-      "@next/next/no-page-custom-font": "off",
-      "@typescript-eslint/no-explicit-any": "off",
-      "@next/next/no-typos": "off",
-    },
-  }),
-];
+const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, ...compat.config({
+  rules: {
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@next/next/no-typos": "off",
+  }
+}), {
+  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+}];
 
 export default eslintConfig;
